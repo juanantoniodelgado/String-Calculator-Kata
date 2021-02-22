46856cf7e9c012a2e201f2db95e352e751b88e4c
Juan Antonio <juanantonio.delgadolpez@gmail.com>
2021-02-22 14:37:30 +0100

    Step 2 completed

--- tests/Unit/Application/AddNumbersTest.php
+++ tests/Unit/Application/AddNumbersTest.php
@@ -14,7 +14,15 @@ class AddNumbersTest extends TestCase
     /**
      * @test
      */
-    public function testAddition()
+    public function testMultipleAddition()
+    {
+        $this->assertEquals(3, $this->service->add('1,1,1'));
+    }
+
+    /**
+     * @test
+     */
+    public function testSingleAddition()
     {
         $this->assertEquals(2, $this->service->add('1,1'));
     }
